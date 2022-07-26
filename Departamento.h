0dfcecf502fa52e77ce8d14d0377e1daf69e89f9
Jonathan <many.d.r.o.2001@hotmail.com>
2022-07-25 23:39:36 -0500

    declaracion de metodos eliminar u, cantidad u, mostrar u

--- Departamento.h
+++ Departamento.h
@@ -1,9 +1,15 @@
 #ifndef DEPARTAMENTO_H
 #define DEPARTAMENTO_H
-
+#include "Universidad.h"
 class Departamento
 {
+    Universidad *uniStart;
+
 public:
+    Departamento();
+    void eliminarUniversidad();
+    int cantidadUniversidades();
+    void mostrarUniversidades();
 };
 
 #endif
