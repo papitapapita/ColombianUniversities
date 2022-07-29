e1d6156dd20a8bfb9257aff4df6baddf25692686
Jonathan <many.d.r.o.2001@hotmail.com>
2022-07-28 22:03:42 -0500

    se crearon los atributos y metodos basicos de estudiantes

--- Estudiante.h
+++ Estudiante.h
@@ -1,9 +1,39 @@
 #ifndef ESTUDIANTE_H
 #define ESTUDIANTE_H
+#include <iostream>
+#include "nota.cpp"
+using namespace std;
 
 class Estudiante
 {
+    string nombres;
+    string tipoDocumento;
+    string apellidos;
+    string genero;
+    int edad;
+    int documento;
+    Estudiante *next;
+    Nota *start;
+
 public:
+    Estudiante();
+    Estudiante(string nombres, string tipoDocumento, string apellidos, string genero, int edad, int documento);
+    string getNombres();
+    string getTipoDocumento();
+    string getApellidos();
+    string getGenero();
+    int getEdad();
+    int getDocumento();
+    Estudiante *getNext();
+    Nota *getStart();
+    void setNombres(string);
+    void setTipoDocumento(string);
+    void setApellidos(string);
+    void setGenero(string);
+    void setEdad(int);
+    void setDocumento(int);
+    void setNext(Estudiante *);
+    void setStart(Nota *);
 };
 
 #endif
--- estudiante.cpp
+++ estudiante.cpp
@@ -1,1 +1,117 @@
 #include "Estudiante.h"
+
+Estudiante::Estudiante()
+{
+    nombres = "";
+    tipoDocumento = "";
+    apellidos = "";
+    genero = "";
+    edad = 0;
+    documento = 0;
+    next = NULL;
+    start = NULL;
+}
+
+Estudiante::Estudiante(string nombres, string tipoDocumento, string apellidos, string genero, int edad, int documento)
+{
+    this->nombres = nombres;
+    this->tipoDocumento = tipoDocumento;
+    this->apellidos = apellidos;
+    this->genero = genero;
+    this->edad = edad;
+    this->documento = documento;
+    this->next = next;
+    this->start = start;
+}
+
+Estudiante::~Estudiante()
+{
+    Nota *aux = this->getStart();
+    Nota *aux2;
+    while (aux)
+    {
+        aux2 = aux;
+        aux = aux->getNext();
+        delete aux2;
+    }
+}
+
+string Estudiante::getNombres()
+{
+    return nombres;
+}
+
+string Estudiante::getTipoDocumento()
+{
+    return tipoDocumento;
+}
+
+string Estudiante::getApellidos()
+{
+    return apellidos;
+}
+
+string Estudiante::getGenero()
+{
+    return genero;
+}
+
+int Estudiante::getEdad()
+{
+    return edad;
+}
+
+int Estudiante::getDocumento()
+{
+    return documento;
+}
+
+Estudiante *Estudiante::getNext()
+{
+    return next;
+}
+
+Nota *Estudiante::getStart()
+{
+    return start;
+}
+
+void Estudiante::setNombres(string nombres)
+{
+    this->nombres = nombres;
+}
+
+void Estudiante::setTipoDocumento(string tipoDocumento)
+{
+    this->tipoDocumento = tipoDocumento;
+}
+
+void Estudiante::setApellidos(string apellidos)
+{
+    this->apellidos = apellidos;
+}
+
+void Estudiante::setGenero(string genero)
+{
+    this->genero = genero;
+}
+
+void Estudiante::setEdad(int edad)
+{
+    this->edad = edad;
+}
+
+void Estudiante::setDocumento(int documento)
+{
+    this->documento = documento;
+}
+
+void Estudiante::setNext(Estudiante *next)
+{
+    this->next = next;
+}
+
+void Estudiante::setStart(Nota *start)
+{
+    this->start = start;
+}
