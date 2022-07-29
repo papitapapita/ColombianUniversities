e9eed517ff7a9b214a04377568e24cb90c99d6a0
Jonathan <many.d.r.o.2001@hotmail.com>
2022-07-28 23:15:31 -0500

    se crearon los atributos y metodos basicos de programa

--- Programa.h
+++ Programa.h
@@ -1,8 +1,31 @@
 #ifndef PROGRAMA_H
 #define PROGRAMA_H
+#include <iostream>
+#include "estudiante.cpp"
 
+using namespace std;
 class Programa
 {
+    string nombre;
+    string area;
+    int semestres;
+    Programa *next;
+    Estudiante *start;
+
+public:
+    Programa();
+    Programa(string, string, int);
+    ~Programa();
+    string getNombre();
+    string getArea();
+    int getSemestres();
+    Programa *getNext();
+    Estudiante *getStart();
+    void setNombre(string);
+    void setArea(string);
+    void setSemestres(int);
+    void setNext(Programa *);
+    void setStart(Estudiante *);
 };
 
 #endif
--- programa.cpp
+++ programa.cpp
@@ -1,1 +1,76 @@
 #include "Programa.h"
+
+Programa::Programa()
+{
+    nombre = area = "";
+    semestres = 0;
+}
+
+Programa::Programa(string nombre, string area, int semestres)
+{
+    this->nombre = nombre;
+    this->area = area;
+    this->semestres = semestres;
+}
+
+Programa::~Programa()
+{
+    Estudiante *aux = this->getStart();
+    Estudiante *aux2;
+    while (aux)
+    {
+        aux2 = aux;
+        delete aux2;
+        aux = aux->getNext();
+    }
+}
+
+string Programa::getNombre()
+{
+    return nombre;
+}
+
+string Programa::getArea()
+{
+    return area;
+}
+
+int Programa::getSemestres()
+{
+    return semestres;
+}
+
+Programa *Programa::getNext()
+{
+    return next;
+}
+
+Estudiante *Programa::getStart()
+{
+    return start;
+}
+
+void Programa::setNombre(string nombre)
+{
+    this->nombre = nombre;
+}
+
+void Programa::setArea(string area)
+{
+    this->area = area;
+}
+
+void Programa::setSemestres(int semestres)
+{
+    this->semestres = semestres;
+}
+
+void Programa::setNext(Programa *next)
+{
+    this->next = next;
+}
+
+void Programa::setStart(Estudiante *start)
+{
+    this->start = start;
+}
