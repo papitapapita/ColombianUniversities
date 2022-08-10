fa940c4143e84759b6923194766a553f0477b9c7
Migue <Bucki28>
2022-08-10 11:14:51 -0500

    demandaCarr-Prev

--- Multilist.cpp
+++ Multilist.cpp
@@ -154,6 +154,26 @@ void Multilist::porcenEdades()
 
 void Multilist::demandaCarr()
 {
+    Universidad *uniX = this -> getHead();
+    while(uniX)
+    {
+        cout << "------"<< uniX ->getNombre() << "------" << endl;
+        Sede *sedeX = uniX ->getStart();
+        while(sedeX)
+        {
+            Programa *progX = sedeX ->getStart();
+            
+            while(progX)
+            {
+                
+                cout << progX -> getNombre() << " Demanda de " << progX->cantidadEstudiantes() << endl ;
+                progX = progX ->getNext();
+            }
+            sedeX = sedeX->getNext();
+        }
+        uniX = uniX -> getNext();
+    }
+
 }
 
 void Multilist::porcentGenero()
@@ -250,6 +270,6 @@ void Multilist::ordenarUniversidades()
             cout << "2" <<endl;
         }
            
-     }
-     cout<<"\nuniversidad  ordenada..."<<endl;
+     }                                                                                          
+    cout<<"\nUniversidad  ordenada..."<<endl;
 }
